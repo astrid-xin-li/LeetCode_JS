391497e565acdd529c70679f8f8d228e7e053ac4
jinjin <1710026942@qq.com>
2022-07-20 10:16:46 +0800

    完成罗马数字转换功能

--- math/13.roman-to-integer.js
+++ math/13.roman-to-integer.js
@@ -20,13 +20,31 @@ const symbol = {
  * @return {number}
  */
 var romanToInt = function(s) {
+    const arr = Array.from(s);
     // 这里又个特殊点左边要是比有右边小，那么就需要 拿右边的值去减去左边的值
-    Array.from(s).reduce(() => {
-
+    const {value: num} = arr.reduceRight(({value, keyName}, name) => {
+        if (!symbol[name]) {
+            // 用户传入的内容有些问题，直接跳过这个
+            return {
+                value,
+                keyName,
+            }
+        }
+        let num = symbol[name] + value;
+        if (symbol[keyName] && symbol[keyName] > symbol[name]) {
+            // 当前是右边有内容，需要去比较一下，是否小于右侧
+            num = value - symbol[name];
+        }
+        return {
+            keyName: name,
+            value: num,
+        }
+        
     }, {
         value: 0,
-        
+        keyName: '',
     })
+    return num;
 };
 // @lc code=end
 
