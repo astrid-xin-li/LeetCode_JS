dc5c7a901c12d9c9c1bf1899d3fd76bb4800b6c5
jinjin <1710026942@qq.com>
2022-08-03 11:49:18 +0800

    feat: 求给定字母中组合回文最长长度——要注意map的长度是size不是 length

---
+++ leetcode_75/409.longest-palindrome.ts
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=409 lang=typescript
+ *
+ * [409] Longest Palindrome
+ */
+
+// @lc code=start
+function longestPalindrome(s: string): number {
+    // 回文是有无数个 偶数个相同字母组成，最多含有一个字母是 奇数
+    let palindromeLength = 0;
+    const map = new Map();
+    Array.from(s).map(value => {
+        if (map.get(value)) {
+            // 已经有了一个，那加上这个就两个了，凑一对，并删除方便下次记录
+            map.delete(value);
+            palindromeLength = palindromeLength + 2;
+        } else {
+            map.set(value, 1);
+        }
+    })
+    if (map.size) {
+        // 现在map里面存储的就是单个字母的，那么回文里面最多只能有一个奇数，所以加上这个就行了
+        palindromeLength ++;
+    }
+    return palindromeLength;
+};
+// @lc code=end
+
