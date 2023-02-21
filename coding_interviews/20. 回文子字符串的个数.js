52f3a8e5eed2fb6b54ff003a939f3e1326870420
jinjin <1710026942@qq.com>
2023-02-21 19:09:10 +0800

    考虑一个字符串有多少回文时，要分两类，一类奇数个回文，一类是偶数个回文

--- coding_interviews/20. 回文子字符串的个数.js
+++ coding_interviews/20. 回文子字符串的个数.js
@@ -8,7 +8,39 @@
  * @return {number}
  */
  var countSubstrings = function(s) {
-
+    let result = 0;
+    // 将 s 中 各个元素作为中心点，依次向外扩展查找扩展外的两个元素是否一样，若发现不一样的直接终止查询 此元素作为中心点的回文已经结束
+    for (let index = 0; index < s.length; index++) {
+        let left = index - 1;
+        let right = index + 1;
+        // 当前元素默认就是一个回文，所以记录应该从 1 开始
+        let currentNum = 1;
+        while(left >= 0 && right < s.length) {
+            // 回文为奇数
+            if (s[left] === s[right]) {
+                currentNum += 1;
+                left -= 1;
+                right += 1;
+            } else {
+                break;
+            }
+        }
+        // 上述循环是查的是以 index 作为 中心点进行回文对齐，下面需要找的是 index 和 index + 1 之间作为中心点，进行循环查找
+        left = index;
+        right = index + 1;
+        while(left >= 0 && right < s.length) {
+            // 回文为偶数
+            if (s[left] === s[right]) {
+                currentNum += 1;
+                left -= 1;
+                right += 1;
+            } else {
+                break;
+            }
+        }
+        result += currentNum;
+    }
+    return result;
 };
 
 //  输入：s = "abc"
