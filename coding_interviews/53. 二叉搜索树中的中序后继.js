ee137132011dbc6fd27b99ae9cc6d96dc7ce0058
jinjin <1710026942@qq.com>
2023-05-17 10:37:01 +0800

    查找中序下一个节点时，需要注意右孩子问题

--- coding_interviews/53. 二叉搜索树中的中序后继.js
+++ coding_interviews/53. 二叉搜索树中的中序后继.js
@@ -17,7 +17,45 @@
  * @return {TreeNode}
  */
 var inorderSuccessor = function(root, p) {
-    
+    // 二叉搜索树，只当前节点左孩子都会比自身小，右孩子都会比自身大
+    // 而 中序后继 是指按中序遍历p的下一个节点
+    // 左 中 右
+    const target = p.val;
+    const list  = [];
+    const searchLeftNode = (node) => {
+        if (node.left) {
+            return searchLeftNode(node.left);
+        }
+        return node;
+    }
+
+    const inorderTraversal = (node) => {
+        // 若当 p 小于当前节点， 则在左边 （左子树的左边，求左子树根，左子树右边，求左子树的父节点）
+        // 大于当前节点，则在右边 （右子树的左边，求右子树根，右子树右边，求右子树的父节点的父节点）
+        // 所以，当前是左孩子则存当前dom，若是右孩子则只存右孩子
+        if (!node) {
+            return null;
+        }
+        const val = node.val;
+        if (val === target) {
+            if (node.right) {
+                return searchLeftNode(node.right);
+            }
+            return list.pop() ?? null;
+        }
+        if (val > target) {
+            list.push(node);
+            if (node.left) {
+                return inorderTraversal(node.left);
+            }
+            return null;
+        }
+        if (node.right) {
+            return inorderTraversal(node.right);
+        }
+        return null;
+    }
+    return inorderTraversal(root);
 };
 
 // https://leetcode.cn/problems/P5rCT8/?envType=study-plan&id=lcof-ii&plan=lcof&plan_progress=bv0usar
@@ -40,3 +78,15 @@ var inorderSuccessor = function(root, p) {
 // -105 <= Node.val <= 105
 // 树中各节点的值均保证唯一。
 //  
+t = {
+    val: 5,
+    right: { val: 6, },
+    left: {
+        val: 3,
+        right: { val: 4 },
+        left: {
+            val: 2,
+            left: { val: 1},
+        }
+    }
+}
